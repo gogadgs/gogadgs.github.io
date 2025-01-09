c200c267fef6cb5f12586fcf7cdd8365e7f9d73f
douglas godois dos santos <doug_potter92@hotmail.com>
2025-01-09 00:11:58 -0300

    terceiro commit

--- src/JS/index.js
+++ src/JS/index.js
@@ -8,9 +8,40 @@ const changeTheme = () => {
      : rootHtml.setAttribute("data-theme", "dark");
     toggleTheme.classList.toggle("bi-brightness-high");
     toggleTheme.classList.toggle("bi-moon-stars");
-
+    localStorage.setItem("theme", currenTheme);
 };
 toggleTheme.addEventListener("click",changeTheme);
+
+
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach(header=>{
+    header.addEventListener("click",()=>{
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item=>{
+    item.addEventListener("click",()=>{
+        menuLinks.forEach(i=>
+            i.classList.remove("active"));
+            item.classList.add("active");
+    })
+})
+
+
+
+const saveThemeToLocalStorage = () => {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+    
+};
 
 
 
